9e370e174ba6be0e12b33d626534bf7e95718b8e
Omnissiah <234093135+Zeta1313@users.noreply.github.com>
2026-05-22 00:17:28 -0700

    Created files needed and filled out the FileReader, currently just an extension class

---
+++ src/main/java/io/github/Zeta1313/centroidfinder/CSVExport.java
@@ -0,0 +1,5 @@
+package io.github.Zeta1313.centroidfinder;
+
+public class CSVExport {
+    
+}
---
+++ src/main/java/io/github/Zeta1313/centroidfinder/VideoSummaryApp.java
@@ -0,0 +1,11 @@
+package io.github.Zeta1313.centroidfinder;
+
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.PrintWriter;
+import java.util.List;
+import javax.imageio.ImageIO;
+
+public class VideoSummaryApp {
+    
+}
