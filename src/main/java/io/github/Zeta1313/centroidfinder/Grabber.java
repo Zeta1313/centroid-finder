1ba317973b960040387fb09584efe829bd418f52
Omnissiah <234093135+Zeta1313@users.noreply.github.com>
2026-05-22 02:01:00 -0700

    Added JCODEC dependencies and code, this should be all we need

---
+++ src/main/java/io/github/Zeta1313/centroidfinder/Grabber.java
@@ -0,0 +1,29 @@
+package io.github.Zeta1313.centroidfinder;
+
+import java.awt.image.BufferedImage;
+import java.io.File;
+
+import org.jcodec.api.FrameGrab;
+import org.jcodec.common.io.NIOUtils;
+import org.jcodec.common.model.Picture;
+import org.jcodec.scale.AWTUtil;
+
+public class Grabber {
+
+    private FrameGrab grab;
+
+    public Grabber(String path) throws Exception {
+
+        grab = FrameGrab.createFrameGrab(
+                NIOUtils.readableChannel(new File(path))
+        );
+    }
+
+    public BufferedImage nextFrame() throws Exception {
+        Picture picture = grab.getNativeFrame();
+        if (picture == null) {
+            return null;
+        }
+        return AWTUtil.toBufferedImage(picture);
+    }
+}
