747afe58cf70bfd084f5cae218096254da6a2df7
Omnissiah <234093135+Zeta1313@users.noreply.github.com>
2026-05-27 22:44:52 -0700

    Videos path completed

--- server/server.js
+++ server/server.js
@@ -1,10 +1,35 @@
 import express from "express"
+import dotenv from "dotenv"
+
+dotenv.config()
+
+const app = express()
 
 application.use(express.json());
 
 app.get("/api/videos", async (req, res) => {
+    try {
+        const videosPath = process.env.VIDEOS_PATH
 
-});
+        if (!videosPath) {throw new Error("Error reading video directory")}
+
+        const absolutePath = path.resolve(videosPath)
+
+        const files = await fs.readdir(absolutePath)
+
+        const videoFiles = files.filter(file => {
+            const extension = path.extname(file).toLowerCase()
+            return allowedExtensions.includes(extension)
+        })
+
+        res.json(videoFiles)
+    }
+    catch {
+        res.status(500).json({
+            error: "Error reading video directory"
+        })
+    }
+})
 
 app.get("/thumbnail/{filename}", async (req, res) => {
 
@@ -15,5 +40,9 @@ app.post("/process/{filename}", async (req, res) => {
 });
 
 app.get("/process/{jobId}/status", async (req, res) => {
-    
-})
+
+});
+
+app.listen(3000, () => {
+    console.log("Server running on port 3000")
+});
